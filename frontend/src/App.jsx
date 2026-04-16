ef03f5d7aea26809877319b106886a7ea6effb8c
RobertCavero <robertcavero404@gmail.com>
2026-04-16 19:44:29 -0300

    fixes: fixing some server logic and error handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+
 import "./App.css";
-import Cards from "./Components/Cards/Cards";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      {/* Featured Cards Section */}
-      <Cards />
-
-      {/* Hero Section */}
-      <section id="center" className="hero-section">
-        <div className="hero">
-          <img
-            src={heroImg}
-            className="base"
-            width="170"
-            height="179"
-            alt="Hero"
-          />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-
+    <section>
         {/* Info Section */}
         <div className="info">
           <h1>Get started</h1>
